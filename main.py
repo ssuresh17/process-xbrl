a8e6ec093a95e88960ee8bb1f37c4dd5ff17d7c1
ssuresh17 <160567052+ssuresh17@users.noreply.github.com>
2024-03-25 13:43:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ Last updated: Feb 2024, K. Wheelan
 import subprocess
 import sys
 import os
-
 import argparse # commandline parsing
 import pandas as pd # data manipulation
 from jinja2 import Environment, FileSystemLoader # html formating
@@ -23,13 +22,17 @@ from utils.Sheet import Sheet
 from utils.Acfr import Acfr
 from utils.constants import * #all global variables
 from utils.helper_functions import clean
+from utils.word_comments import ExtractComments
 
 # flask dependencies
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from werkzeug.utils import secure_filename
 import gettext, shlex
 
 from bs4 import BeautifulSoup
+
+import mammoth
+import string
 
 # =============================================================
 # Constants
@@ -84,7 +87,6 @@ def parse_contexts(contexts_file : str) -> Dict[str, Dict[str, str]]:
     dictionaries for later
     """   
     # open contexts excel sheet
-    pd.read_excel(contexts_file)
     try:
         contexts = pd.read_excel(contexts_file)
     except:
@@ -106,17 +108,25 @@ def parse_contexts(contexts_file : str) -> Dict[str, Dict[str, str]]:
 def write_html(input_file : str,
                output_file : str,
                context_name_map : Dict[str, Dict[str, str]],
-               format : str):
-    """ Create inline xbrl document and save as an html file at {output_file} location """
+               format : str,
+               wordfiles: Dict[str, Dict[str, str]]):
+    """ Create inline xbrl document and save as an htm`l file at {output_file} location """
     # iterate through sheets, saving Sheet() objects
     input_xl = pd.ExcelFile(input_file)
     acfr = Acfr([Sheet(input_file, sheet_name, context_name_map) for sheet_name in input_xl.sheet_names if sheet_name != "Label Dropdowns"])
 
+    #extracted the contents from word
+    cover_page, _ = extract_text_and_images_from_docx(wordfiles['cover'])
+    auditors_letter, _ = extract_text_and_images_from_docx(wordfiles['auditor'])
+    notes, _ = extract_text_and_images_from_docx(wordfiles['notes'])
+    supplementary_info, _ = extract_text_and_images_from_docx(wordfiles['info'])
+    statistical_section, _ = extract_text_and_images_from_docx(wordfiles['statistic'])
+
     # Load the template and render with vars
-    rendered_ixbrl = render_template('xbrl/base.html', acfr = acfr, format = format)
+    rendered_ixbrl = render_template('xbrl/base.html', acfr = acfr, format = format,cover_page=cover_page, auditors_letter=auditors_letter,  notes=notes, supplementary_info=supplementary_info, statistical_section=statistical_section) 
 
     # Save the rendered template to output file
-    with open(output_file, 'w') as write_location:
+    with open(output_file, 'w', encoding="utf8") as write_location:
         write_location.write(rendered_ixbrl)
 
 # def load_dependencies():
@@ -151,12 +161,15 @@ def create_viewer_html(output_file : str,
     # Also add the Arelle.arelle directory inside dependencies 
     arelle_dir = os.path.join(base_dir, "dependencies", "Arelle")
     sys.path.append(arelle_dir)
-
-    from arelle import CntlrCmdLine
+    
+    from arelle import CntlrCmdLine 
     from arelle.Locale import setApplicationLocale
-
     # command to run Arelle process
     plugins = os.path.join(ROOT, "dependencies", "ixbrl-viewer", "iXBRLViewerPlugin")
+    
+    viewer_filepath=viewer_filepath.replace("\\",'/') # added this for solve some errors in file directory  
+    plugins=plugins.replace("\\",'/') # added this for solve some errors in file directory  
+    # print(viewer_filepath,plugins)
     viewer_url = "https://cdn.jsdelivr.net/npm/ixbrl-viewer@1.4.8/iXBRLViewerPlugin/viewer/dist/ixbrlviewer.js"
     args = f"--plugins={plugins} -f {output_file} --save-viewer {viewer_filepath} --viewer-url {viewer_url}"
     
@@ -166,7 +179,7 @@ def create_viewer_html(output_file : str,
     CntlrCmdLine.parseAndRun(args)
 
     # Read in the generated HTML
-    with open(viewer_filepath, 'r') as file:
+    with open(viewer_filepath, 'r', encoding="utf8") as file:
         html_content = file.read()
 
     # Parse the HTML with BeautifulSoup
@@ -179,13 +192,59 @@ def create_viewer_html(output_file : str,
         script_tag['src'] = '{{ url_for(\'static\', filename=\'js/ixbrlviewer.js\') }}'
 
     # Write the modified HTML back out
-    with open(viewer_filepath, 'w') as file:
+    with open(viewer_filepath, 'w', encoding="utf8") as file:
         file.write(str(soup))
 
-    os.rename('templates/site/ixbrlviewer.js', 'static/js/ixbrlviewer.js')
+    # os.rename('templates/site/ixbrlviewer.js', 'static/js/ixbrlviewer.js')
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS  
+
+
+# added the function for get the word conntent 
+def extract_text_and_images_from_docx(file_path):
+    
+    from utils.constants import p_id
+
+    result = mammoth.convert_to_html(file_path)
+    html = result.value  # Extracted HTML content
+    images = result.messages  # Extracted images, if any    
+    updated_html =''
+    soup = BeautifulSoup(html,"html.parser")
+    p_html =[]
+    # Remove a tags    
+    for a in soup.find_all(['a']):
+        a.decompose()
+
+    for p_tag in soup.find_all('p'):
+        validated =True
+        
+        if p_tag.text !='':
+            for char in p_tag.text:
+                if char in string.ascii_letters:
+                    validated = False 
+                    break
+
+            if validated:        
+                p_html.append(p_tag)
+    result = ExtractComments.get_comments_and_text(file_path,html)
+    if result: 
+        for  i in range (0,len(result['comments'])):
+            comment, selected_text, p_count =result['comments'][i],result['selected_text'][i],result['count'][i]
+            context_id = result['context_id'][i]
+            p_html[p_count].replace_with(f'''\n\n<ix:nonFraction contextRef="{context_id}" name="acfr:{comment}" unitRef="pure" id="p{p_id}" decimals="0" format="ixt:num-dot-decimal" >
+    {selected_text}
+</ix:nonFraction>\n\n''')
+            p_id +=1
+            
+            
+        updated_html = str(soup)
+        updated_html = updated_html.replace('&gt;', '>')
+        updated_html = updated_html.replace('&lt;', '<')           
+        return updated_html,images
+    
+    updated_html = str(soup)
+    return updated_html, images
 
 # =============================================================
 # Flask
@@ -195,26 +254,33 @@ def allowed_file(filename):
 def home():
     return render_template('site/home.html', loading=True)
 
-
 @app.route('/viewer')
 def view(viewer_file_name = "site/viewer.html"):
-    return render_template(viewer_file_name)
-
+    return render_template(viewer_file_name )
 
 @app.route('/upload', methods=['POST'])
 def upload_file(output_file = "static/output/output.html", format = "gray"):
     if 'file' not in request.files:
         return "No file"
     
-    file = request.files['file']
+    wordfiles={}
+    # Example: Print the file names
+    wordfiles['cover']= request.files['coverfile']
+    wordfiles['auditor']= request.files['auditorfile']
+    wordfiles['notes']= request.files['notesfile']
+    wordfiles['info']= request.files['infofile']
+    wordfiles['statistic']= request.files['statisticalfile']
 
+    file = request.files['file']
+    
     if file.filename == '':
         return 'No selected file'
-        
+    
     if file and allowed_file(file.filename):
-        write_html(file, output_file, context_name_map, format)
+        write_html(file, output_file, context_name_map, format,wordfiles)
         viewer_file_name = "templates/site/viewer.html"
         create_viewer_html(output_file, viewer_file_name)
+
         return render_template("site/upload.html")
     else:
         return 'Invalid file type'
